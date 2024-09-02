1bb5e0ac99a5ad414d9890fa936ae8d20b6fb85e
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-02 14:33:49 +0530

    variables

---
+++ 06-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FRUITS=("Apple" "KIWI" "ORANGE") #Array
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+echo "All fruits: ${FRUITS[@]}"
