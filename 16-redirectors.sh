fcbbd2f5ac72116da465e0a7cbae6d2deaff6f3d
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-03 14:42:08 +0530

    datatypes

---
+++ 16-redirectors.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+echo "user id is: $USERID"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failed $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G success $N" &>>$LOG_FILE
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "please run the script with root priveleges" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+CHECK_ROOT
+
+for package in $@
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then    
+        echo "$package not installed...going to install it" &>>$LOG_FILE
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo "package is already installed" &>>$LOG_FILE
+    fi
+done
