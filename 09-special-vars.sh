a9f8c7d2256cc860f2fd74a733577e5df6018b9e
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-03 10:12:01 +0530

    datatypes

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,4 +5,5 @@ echo "script name: $0"
 echo "current working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "process instance ID of the script executing now: $$"
+sleep 100 &
 echo "process instance ID of last background command: $!"
