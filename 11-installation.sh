c26971871bc54966838ccf135aa4c3eefcfccb2e
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-03 10:36:06 +0530

    datatypes

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-USERID=$(id -u)
-echo "user id is: $USERID"
-if [ $USERID -ne 0 ]
-then
-    echo "please run the script with root priveleges"
-fi
-dnf install git -y
+# USERID=$(id -u)
+# echo "user id is: $USERID"
+# if [ $USERID -ne 0 ]
+# then
+#     echo "please run the script with root priveleges"
+# fi
+# dnf install git -y
+
+ls -ltr
+
+lsdsdg -ltr
+
+ls -l
