cc627abb252b3ba1f85c4b51b0c7361bd0567819
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-03 13:44:27 +0530

    datatypes

--- 12-functions.sh
+++ 12-functions.sh
@@ -14,11 +14,16 @@ VALIDATE(){
     fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "please run the script with root priveleges"
-    exit 1
-fi
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "please run the script with root priveleges"
+        exit 1
+    fi
+}
+
+CHECK_ROOT
+
 dnf list installed git
 
 
