e99345ae4fda4df76b6df92ec708b9e4b895e333
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-03 10:44:40 +0530

    datatypes

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,6 +7,11 @@ then
     echo "please run the script with root priveleges"
     exit 1
 fi
-dnf install git -y
-
-
+dnf list installed git
+if [ $? -ne 0 ]
+then    
+    echo "git is not installed"
+    dnf install git -y
+else
+    echo "git is already installed"
+fi
