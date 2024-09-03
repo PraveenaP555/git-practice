2038660c4e8352e153f7824a3419f07dae084f2c
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-03 13:51:02 +0530

    datatypes

---
+++ 13-colors.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+echo "user id is: $USERID"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is $R failed $N"
+        exit 1
+    else
+        echo "$2 is $G success $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "please run the script with root priveleges"
+        exit 1
+    fi
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+
+
+if [ $? -ne 0 ]
+then    
+    echo "git is not installed"
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "git is already installed"
+fi
+
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "mysql is already installed"
+fi
