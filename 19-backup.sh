cf8ac60519f18d3c4d624c8ac8330f054e1bfb30
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-05 14:54:39 +0530

    praveena

--- 19-backup.sh
+++ 19-backup.sh
@@ -18,12 +18,12 @@ then
     USAGE
 fi
 
-if [ ! -d $SOURCE_DIR]
+if [ ! -d $SOURCE_DIR ]
 then
     echo "$SOURCE_DIR does not exist....please check"
 fi     
 
-if [ ! -d $DEST_DIR]
+if [ ! -d $DEST_DIR ]
 then
     echo "$DEST_DIR does not exist....please check"
 fi    
