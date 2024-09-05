c8b83b4debe64405c07d574b5d0e43334e58c712
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-05 11:02:01 +0530

    praveena

--- 17-set.sh
+++ 17-set.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 
-set -ex #setting automatic exit if we get error, x is for debug
+set -ex # setting automatic exit if we get error, x is for debug
+failure(){
+    echo "failed at:  $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal
 echo "hello world success"
 echoooo "hello world failure"
 echo "hello world after failure"
