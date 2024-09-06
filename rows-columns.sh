d19c5c905d34e61ad39d4b3e6ca905ec3626dddd
PraveenaP <praveena.chowdhary28@gmail.com>
2024-09-06 14:15:06 +0530

    disk usage

---
+++ rows-columns.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#READ_FILE=$(cat /home/ec2-user/file.txt)
+
+
+
+# Input file
+input_file="/home/ec2-user/file.txt"
+# Temporary files
+temp_file1=$(mktemp)
+temp_file2=$(mktemp)
+
+# Transpose the rows to columns
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        matrix[NR, i] = $i
+    }
+    n = NF
+} 
+END {
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= NR; j++) {
+            if (j > 1) printf " "
+            printf "%s", matrix[j, i]
+        }
+        printf "\n"
+    }
+}' "$input_file" > "$temp_file1"
+
+# Move the result to the output file
+mv "$temp_file1" "transposed_$input_file"
+
+# Clean up temporary files
+rm -f "$temp_file2"
